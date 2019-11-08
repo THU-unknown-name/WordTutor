a4f4bcda220deddb929b7b4ccac64ff2362a8ddc
sjlcwn <sjlcwn@126.com>
2019-11-08 21:14:22 +0800

    Add Sentence&modify extra info to dict

--- WordDict/Extract.py
+++ WordDict/Extract.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python3
 import os
+import re
 
 def Extract(lList):
-	return [[lList[1],lList[0]],[lList[2]]]
+	lSentences=[]
+	lSplit=re.split("[\r\n]+",lList[3])
+
+	pSplit=re.compile("^[\\d\\.]+$")
+	pEng=re.compile("^[-_+=~`!@#$%^&*()\\[\\]\\{\\}|\;:'\",./<>? A-Za-z]+$")
+
+	sSpace=""
+	lData=[None,None]
+	for lLine in lSplit:
+		if pSplit.fullmatch(lLine) is not None:
+			if lData[0] is not None and lData[1] is not None:
+				lSentences.append(lData)
+			lData=[None,None]
+		elif pEng.fullmatch(lLine) is not None:
+			if lData[0] is None:
+				lData[0]=lLine
+			else:
+				lData[0]+="\n"+lLine
+		elif len(lLine)>0:
+			if lData[1] is None:
+				lData[1]=lLine
+			else:
+				lData[1]+="\n"+lLine
+
+	if lData[0] is not None and lData[1] is not None:
+		lSentences.append(lData)
+	lData=[None,None]
+
+	return [[lList[1],lList[0]],{"hennkou":lList[2],"reiku":lSentences}]
 
 def ExtractToDir(lList,rootpath):
 	with open(os.path.join(rootpath,"WordList.txt"),"w") as fList:
