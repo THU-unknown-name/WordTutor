d47d08d225fb4788041e448369b5f04975af3d79
forchchch <55648694+forchchch@users.noreply.github.com>
2019-12-17 11:57:46 +0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -37,54 +37,73 @@ class look_up(QtWidgets.QMainWindow):
         self.retranslateUi(self)
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 560)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.suggestion_label = QtWidgets.QLabel(self.centralwidget)
-        self.suggestion_label.setGeometry(QtCore.QRect(189, 100, 71, 30))
+        self.suggestion_label.setGeometry(QtCore.QRect(100, 100, 100, 30))
         font = QtGui.QFont()
-        font.setFamily("华文琥珀")
-        font.setPointSize(9)
+        font.setFamily("Arial")
+        font.setPointSize(18)
         self.suggestion_label.setFont(font)
         self.suggestion_label.setObjectName("suggestion_label")
+        self.suggestion_label.setStyleSheet('''
+                    QLabel{border:none;color:white;font-weight:700;
+                        }
+                ''')
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(270, 100, 221, 30))
+        self.lineEdit.setGeometry(QtCore.QRect(230, 100, 300, 40))
         self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setFont(QtGui.QFont("Arial", 20))
+
         self.sure_Button = QtWidgets.QPushButton(self.centralwidget)
-        self.sure_Button.setGeometry(QtCore.QRect(520, 100, 60, 30))
+        self.sure_Button.setGeometry(QtCore.QRect(550, 100, 70, 30))
         font = QtGui.QFont()
         font.setFamily("华文细黑")
         self.sure_Button.setFont(font)
         self.sure_Button.setObjectName("sure_Button")
+        self.sure_Button.setStyleSheet('''
+                    QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                ''')
+
         self.browser = QtWidgets.QTextBrowser(self.centralwidget)
         self.browser.setGeometry(QtCore.QRect(190, 150, 400, 300))
         self.browser.setObjectName("my_browser")
 
-        self.browser.setStyleSheet("background-image:url(background.png)")
+        self.browser.setStyleSheet("#QTextBrowser{border-image:url(bak1.jpg);}")
         #self.browser.setWordWrap(True)
         #self.browser.setAlignment(Qt.AlignTop)
         self.exit_Button = QtWidgets.QPushButton(self.centralwidget)
-        self.exit_Button.setGeometry(QtCore.QRect(200, 460, 150, 31))
-        font = QtGui.QFont()
-        font.setFamily("华文细黑")
-        self.exit_Button.setFont(font)
+        self.exit_Button.setGeometry(QtCore.QRect(150, 460, 150, 31))
         self.exit_Button.setObjectName("exit_Button")
+        self.exit_Button.setStyleSheet('''
+                            QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                        ''')
+        self.exit_Button.setIcon(QtGui.QIcon('./exit.jpg'))
 
 
         self.add_Button = QtWidgets.QPushButton(self.centralwidget)
         self.add_Button.setGeometry(QtCore.QRect(440, 460, 150, 31))
-        font = QtGui.QFont()
-        font.setFamily("华文细黑")
-        self.add_Button.setFont(font)
         self.add_Button.setObjectName("add_Button")
+        self.add_Button.setStyleSheet('''
+                                    QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                ''')
 
         self.sound_button = QtWidgets.QPushButton(self.centralwidget)
-        self.sound_button.setGeometry(QtCore.QRect(620, 100, 60, 30))
+        self.sound_button.setGeometry(QtCore.QRect(650, 100, 70, 30))
         font = QtGui.QFont()
         font.setFamily("华文细黑")
         self.sound_button.setFont(font)
         self.sound_button.setObjectName("sound_button")
         self.sound_button.clicked.connect(self.mywordsound)
+        self.sound_button.setStyleSheet('''
+                            QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                        ''')
+        self.sound_button.setIcon(QtGui.QIcon('./sound.jpg'))
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -100,7 +119,7 @@ class look_up(QtWidgets.QMainWindow):
         self.exit_Button.clicked.connect(self.close)
         self.sure_Button.clicked.connect(self.mysearchword)
         self.add_Button.clicked.connect(self.add_to_wordkeeper)
-        MainWindow.setStyleSheet("#MainWindow{border-image:url(background1.jpg);}")
+        MainWindow.setStyleSheet("#MainWindow{border-image:url(bak1.jpg)}")
 
 
     def mywordsound(self):          #发音
@@ -130,10 +149,13 @@ class look_up(QtWidgets.QMainWindow):
         word_to_search = self.lineEdit.text()
         #这里的代码需要根据word_to_search查找相关的解释
         #search_result = "heheda"
-        search_result=self.WORD_DICT.get_info(word_to_search)
+        if word_to_search=="": pass
+        else:
 
-        if search_result==-1:
-            xiu_result = "没有查询到相应的单词\n"
+            search_result=self.WORD_DICT.get_info(word_to_search)
+
+            if search_result==-1:
+                xiu_result = "没有查询到相应的单词\n"
             # max_recorder = 0
             # max_word ="happy"
             # for word,haha in self.WORD_DICT.navigate():
@@ -141,29 +163,29 @@ class look_up(QtWidgets.QMainWindow):
             #     if common_len>max_recorder:
             #         max_word = word
             #         max_recorder = common_len
-            [likelihood, wordlist] = self.WORD_DICT.match_word(word_to_search)
-            top5index = np.argsort(likelihood)[:5]
-            top5word = [wordlist[i] for i in top5index]
-            max_word = top5word[0]
-            xiu_result=xiu_result+"您要查找的是不是下面的单词？\n"+max_word+"\n"
-            temp = self.WORD_DICT.get_info(max_word)
-            #print(temp)
-            xiu_result+=temp[0][1]+"\n"+temp[0][0]+"\n"+temp[1]['hennkou']+"\n"+link(temp[1]['reiku'])
-        else:
-            #print(search_result)
-            xiu_result = search_result[0][1]+"\n"+search_result[0][0]+"\n"+search_result[1]['hennkou']+"\n"+link(search_result[1]['reiku'])
+                [likelihood, wordlist] = self.WORD_DICT.match_word(word_to_search)
+                top5index = np.argsort(likelihood)[:5]
+                top5word = [wordlist[i] for i in top5index]
+                max_word = top5word[0]
+                xiu_result=xiu_result+"您要查找的是不是下面的单词？\n"+max_word+"\n"
+                temp = self.WORD_DICT.get_info(max_word)
+                #print(temp)
+                xiu_result+=temp[0][1]+"\n"+temp[0][0]+"\n"+temp[1]['hennkou']+"\n"+link(temp[1]['reiku'])
+            else:
+                #print(search_result)
+                xiu_result = search_result[0][1]+"\n"+search_result[0][0]+"\n"+search_result[1]['hennkou']+"\n"+link(search_result[1]['reiku'])
 
-        #返回结果为search_result
-        self.browser.setText(xiu_result)
+                #返回结果为search_result
+                self.browser.setText(xiu_result)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "单词搜索"))
-        self.suggestion_label.setText(_translate("MainWindow", "输入单词："))
-        self.sure_Button.setText(_translate("MainWindow", "确定"))
+        self.suggestion_label.setText(_translate("MainWindow", "输入单词"))
         self.exit_Button.setText(_translate("MainWindow", "退出"))
         self.sound_button.setText(_translate("MainWindow", "发音"))
         self.add_Button.setText(_translate("MainWindow", "加入背诵计划"))
+        self.sure_Button.setText(_translate("MainWindow", "确定"))
 
 
 def longestCommonSequence(str_one, str_two, case_sensitive=True):     #寻找最大公有子串
