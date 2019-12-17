3e4378da9563bd02db507fa0cd19924ace8c9e1d
forchchch <55648694+forchchch@users.noreply.github.com>
2019-12-17 11:58:19 +0800

    Update mymainwindow.py

--- mymainwindow.py
+++ mymainwindow.py
@@ -29,8 +29,9 @@ class ErrorWin(object):
         if reply == QtWidgets.QMessageBox.Yes:
             pass
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self, WORD_DICT):
+        super(Ui_MainWindow, self).__init__()
         self.WORD_DICT = WORD_DICT
         self.errorWin = ErrorWin()
 
@@ -40,27 +41,49 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(340, 100, 101, 40))
+        self.label.setGeometry(QtCore.QRect(360, 80, 150, 60))
         font = QtGui.QFont()
         font.setFamily("黑体")
-        font.setPointSize(11)
+        font.setPointSize(20)
         self.label.setFont(font)
         self.label.setObjectName("label")
+        self.label.setStyleSheet('''
+                    QLabel{color:white;font-size:25px;  font-weight:700;
+                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                ''')
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)        #查询单词
-        self.pushButton.setGeometry(QtCore.QRect(340, 150, 120, 41))
+        self.pushButton.setGeometry(QtCore.QRect(320, 150, 150, 60))
+        self.pushButton.setIcon(QtGui.QIcon('./lookup.jpg'))
+        self.pushButton.setStyleSheet('''
+            QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+        ''')
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)      #退出
-        self.pushButton_2.setGeometry(QtCore.QRect(340, 300, 120, 41))
+        self.pushButton_2.setGeometry(QtCore.QRect(320, 360, 150, 60))
         self.pushButton_2.setObjectName("pushButton_2")
-
+        self.pushButton_2.setIcon(QtGui.QIcon('./exit.jpg'))
+        self.pushButton_2.setStyleSheet('''
+                           QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                               font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                       ''')
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)      #背单词
-        self.pushButton_3.setGeometry(QtCore.QRect(340, 200, 120, 41))
+        self.pushButton_3.setGeometry(QtCore.QRect(320, 220, 150, 60))
         self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setIcon(QtGui.QIcon('./recite.jpg'))
+        self.pushButton_3.setStyleSheet('''
+                    QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                ''')
 
         self.game_button = QtWidgets.QPushButton(self.centralwidget)      #游戏
-        self.game_button.setGeometry(QtCore.QRect(340, 250, 120, 41))
+        self.game_button.setGeometry(QtCore.QRect(320, 290, 150, 60))
         self.game_button.setObjectName("game_button")
-
+        self.game_button.setIcon(QtGui.QIcon('./game1.jpg'))
+        self.game_button.setStyleSheet('''
+                           QPushButton{border:none;color:white;font-size:25px;  font-weight:700;
+                               font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                       ''')
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
@@ -73,16 +96,22 @@ class Ui_MainWindow(object):
         self.pushButton_2.clicked.connect(QCoreApplication.instance().quit)
         self.pushButton_3.clicked.connect(self.my_test)
         self.game_button.clicked.connect(self.game_window)
+
+        self.setWindowOpacity(0.9)  # 设置窗口透明度
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置窗口背景透明
         self.retranslateUi(MainWindow)
+
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        MainWindow.setStyleSheet("#MainWindow{border-image:url(background.png);}")
+        MainWindow.setStyleSheet("#MainWindow{border-image:url(bak1.jpg);}")
     def word_search(self):
         self.ui_search=look_up(self.WORD_DICT)
+        #self.ui_search.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
         self.ui_search.show()
     def my_test(self):
         self.ui_recite=ReciteGUI(self.WORD_DICT)
-        if not self.ui_recite.finished:
-            self.ui_recite.show()
+        self.ui_recite.setStyleSheet("#MainWindow{border-image:url(bak1.jpg)}")
+        #self.ui_recite.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
+        self.ui_recite.show()
     def game_window(self):
         vocab = Vocab(self.WORD_DICT)
         word_list_for_game = vocab.get_n_word_from_familiarVocab(8)
@@ -102,13 +131,15 @@ class Ui_MainWindow(object):
         self.ui_game.showDefinition(defCross, defDown)  # 显示中文释义
         self.ui_game.addLabel()
         self.ui_game.addButtons()
+        self.ui_game.setStyleSheet("#MainWindow{border-image:url(bak1.jpg)}")
+        #self.ui_game.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
         self.ui_game.show()
         pass
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "   主菜单"))
+        self.label.setText(_translate("MainWindow", "主菜单"))
         self.pushButton.setText(_translate("MainWindow", "查询单词"))
-        self.pushButton_2.setText(_translate("MainWindow", "退   出"))
-        self.pushButton_3.setText(_translate("MainWindow", "背 单 词"))
+        self.pushButton_2.setText(_translate("MainWindow", "退出"))
+        self.pushButton_3.setText(_translate("MainWindow", "背单词"))
         self.game_button.setText(_translate("MainWindow", "进入游戏"))
