ebe6d767c7c17dec206da97315ba9b3f62513736
wuqian <wu_qi_an@163.com>
2019-12-19 12:26:30 +0800

    add test_StudyPlan to UnitTest

--- UnitTest.py
+++ UnitTest.py
@@ -1,8 +1,10 @@
 from WordDict.UnitTest import Test_WordDict
+from StudyPlan.test_studyPlan import Test_StudyPlan
 import unittest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(Test_WordDict))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(Test_StudyPlan))
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
