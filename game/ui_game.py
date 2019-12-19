485020bc304302a1aecf1ff5297b2d2b8121450c
scorius13 <55903768+scorius13@users.noreply.github.com>
2019-12-19 22:11:51 +0800

    update & clean up

--- game/ui_game.py
+++ game/ui_game.py
@@ -1,14 +1,13 @@
-# 游戏UI界面：目前仅显示空的填词格和中文释义
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from gameSystem import *
-from getBestCrossword import *
-import numpy as np
+from game.gameSystem import *
 
 
 class myQLineEdit(QLineEdit):
+    '''
+    自定义QLineEdit类，可控制光标移动方向
+    '''
     def __init__(self, parent, row, col):
         super(myQLineEdit, self).__init__(parent)
         self.parent = parent
@@ -24,35 +23,42 @@ class myQLineEdit(QLineEdit):
     def focusInEvent(self, QFocusEvent):
         self.parent.current_focus = [self.row, self.col]
         super().focusInEvent(QFocusEvent)
+
     pass
+
 
 class gameWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(gameWindow, self).__init__(*args, **kwargs)
 
-        self.windowTitleChanged.connect(self.onWindowTitleChange)
         self.w_width = 800  # 窗口初始宽度 1000
         self.w_height = 600  # 窗口初始高度 650
         self.w_left = 10  # 窗口起始位置x
         self.w_top = 10  # 窗口起始位置y
-        self.cw_loc = (self.width() / 20, self.w_height / 10)  # 填词格的初始位置 x, y
+        self.gap = 30  # 模块之间、模块与边界的距离
+        self.cw_loc = (0, 0)  # 经过居中调整后的填词格的初始位置
         self.init_cw_len = 30  # 单个格子的默认边长
         self.cw_len = 30  # 单个格子的实际边长
-        self.def_loc = (self.w_width * 1 / 2, self.w_height * 1 / 10)  # 中文释义的初始位置 x, y
+        self.def_loc = (self.w_width * 1 / 2 + 10, 0)  # 中文释义的初始位置 x, y
         self.def_w = self.w_width - self.def_loc[0] - self.w_width / 20
-        self.def_h = 550
+        self.def_h = 500
         self.tbEdgeColor = "rgb(150, 150, 150)"
-        self.btn_top = 500  # 按键的y轴位置
-        # self.textbox = []
+        self.btn_top = 525  # 按键的y轴位置
         self.testMode = False
 
-
-    # 初始化窗口
-    def initUI(self, cw):
+    def initUI(self, cw, WORD_DICT, errorWin):
+        '''
+        # 初始化窗口
+        :param cw: 填词游戏
+        :param WORD_DICT: 词典
+        :param errorWin: 窗口报错接口
+        :return:
+        '''
+        self.WORD_DICT = WORD_DICT
+        self.errorWin = errorWin
         self.cw = cw
         self.setWindowTitle("快乐背单词")
-        # self.setGeometry(self.w_left, self.w_top, self.w_width, self.w_height)
         self.resize(self.w_width, self.w_height)
         self.checkOverlap()
         self.showCrossword()  # 显示空填词格
@@ -60,32 +66,52 @@ class gameWindow(QMainWindow):
         self.addLabel()  # 标号
         self.addButtons()
         self.setObjectName("MainWindow")
-        self.setStyleSheet("#MainWindow{border-image:url(background1.jpg);}")
 
     def addButtons(self):
         '''
         添加按键
         '''
-        self.showAns = QPushButton('显示答案', self)
-        self.showAns.setGeometry(QRect(150, self.btn_top, 100, 41))
-        self.showAns.clicked.connect(self.showAnswer)
-
-        self.hideAns = QPushButton('隐藏答案', self)
-        self.hideAns.setGeometry(QRect(150, self.btn_top, 100, 41))
-        self.hideAns.clicked.connect(self.hideAnswer)
-        self.hideAns.setVisible(False)
-
         self.checkAns = QPushButton('检查答案', self)
-        self.checkAns.setGeometry(QRect(270, self.btn_top, 100, 41))
+        self.checkAns.setGeometry(QRect(220, self.btn_top, 150, 41))
         self.checkAns.clicked.connect(self.checkAnswer)
-
+        self.checkAns.setStyleSheet('''
+                                            QPushButton{border:none;color:white;font-size:25px;font-weight:700;
+                                            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                            ''')
+        self.checkAns.setIcon(QIcon('./exit.jpg'))
+        self.nextGame = QPushButton('下一轮', self)
+        self.nextGame.setGeometry(QRect(390, self.btn_top, 100, 41))
+        self.nextGame.clicked.connect(self.getNextGame)
+        self.nextGame.setStyleSheet('''
+                                            QPushButton{border:none;color:white;font-size:25px;font-weight:700;
+                                            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                            ''')
+        self.nextGame.setIcon(QIcon('./exit.jpg'))
         self.exit = QPushButton('退出', self)
-        self.exit.setGeometry(QRect(390, self.btn_top, 100, 41))
+        self.exit.setGeometry(QRect(510, self.btn_top, 100, 41))
         self.exit.clicked.connect(self.close)
+        self.exit.setStyleSheet('''
+                                            QPushButton{border:none;color:white;font-size:25px;font-weight:700;
+                                            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                            ''')
+        self.exit.setIcon(QIcon('./exit.jpg'))
 
-        # self.nextGame = QPushButton('下一轮', self)
-        # self.nextGame.setGeometry(QRect(510, self.btn_top, 100, 41))
-        # self.nextGame.clicked.connect(self.getNextGame)
+        if self.testMode:
+            self.showAns = QPushButton('显示答案', self)
+            self.showAns.setGeometry(QRect(150, self.btn_top, 100, 41))
+            self.showAns.clicked.connect(self.showAnswer)
+            self.showAns.setStyleSheet('''
+                                        QPushButton{border:none;color:white;font-size:25px;font-weight:700;
+                                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                        ''')
+            self.hideAns = QPushButton('隐藏答案', self)
+            self.hideAns.setGeometry(QRect(150, self.btn_top, 100, 41))
+            self.hideAns.clicked.connect(self.hideAnswer)
+            self.hideAns.setVisible(False)
+            self.hideAns.setStyleSheet('''
+                                        QPushButton{border:none;color:white;font-size:25px;font-weight:700;
+                                        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}
+                                        ''')
 
     def checkOverlap(self):
         '''
@@ -93,32 +119,36 @@ class gameWindow(QMainWindow):
         :return: shift_def_w 中文释义模块需要向右移动的距离
         '''
         MIN_DEF_W = 250
-        GAP = 10
-        lSideCw = self.cw_loc[0]
-        rSideCw = self.cw_loc[0] + self.cw.nCol * self.init_cw_len
-        dSideCW = self.cw_loc[1] + self.cw.nRow * self.init_cw_len
+        lSideCw = 10
+        rSideCw = lSideCw + self.cw.nCol * self.init_cw_len
+        dSideCW = self.cw.nRow * self.init_cw_len
         lSideDef = self.def_loc[0]
 
         # 如果填词游戏和中文释义重叠
         shift_def_w = 0
-        if lSideDef < rSideCw:
-            lapped = rSideCw - lSideDef
-            shift_def_w = self.def_w - max(self.def_w - lapped - GAP, MIN_DEF_W)
+        if lSideDef < rSideCw + self.gap:
+            lapped = rSideCw + self.gap - lSideDef
+            shift_def_w = self.def_w - max(self.def_w - lapped, MIN_DEF_W)
 
         # 如果中文释义模块已达到最小宽度后仍然重叠，缩小填词格的宽度
         new_cw_len_1 = self.init_cw_len
         if self.def_loc[0] + shift_def_w < rSideCw:
-            max_cw_w = self.def_loc[0] - GAP - lSideCw
-
-            new_cw_len_1 = max_cw_w/(self.cw.nCol * self.init_cw_len) * self.init_cw_len
+            max_cw_w = self.def_loc[0] - 2 * self.gap - lSideCw
+            new_cw_len_1 = max_cw_w / (self.cw.nCol * self.init_cw_len) * self.init_cw_len
 
         # 如果填词游戏和按键重叠
         new_cw_len_2 = self.init_cw_len
         if dSideCW > self.btn_top:
-            max_cw_h = self.btn_top - GAP - self.cw_loc[1]
-            new_cw_len_2 = max_cw_h/(self.cw.nRow * self.init_cw_len) * self.init_cw_len
+            max_cw_h = self.btn_top - 2 * self.gap
+            new_cw_len_2 = max_cw_h / (self.cw.nRow * self.init_cw_len) * self.init_cw_len
 
         self.cw_len = min(new_cw_len_1, new_cw_len_2)
+
+        # 填词游戏在左侧居中显示
+        cw_loc_x = (lSideDef + shift_def_w) / 2 - self.cw.nCol * self.cw_len / 2
+        cw_loc_y = self.btn_top / 2 - self.cw.nRow * self.cw_len / 2
+
+        self.cw_loc = (cw_loc_x, cw_loc_y)
 
         return shift_def_w
 
@@ -129,7 +159,7 @@ class gameWindow(QMainWindow):
         cw_height = self.cw.nRow
         cw_width = self.cw.nCol
         my_regex = QRegExp("[a-zA-Z]")
-        flag = [[[]for i in range(cw_width)]for j in range(cw_height)]
+        flag = [[[] for i in range(cw_width)] for j in range(cw_height)]
         self.textbox = [[] for i in range(len(self.cw.sortedList))]
         self.textbox_word = []
         for word in self.cw.sortedList.keys():
@@ -146,7 +176,8 @@ class gameWindow(QMainWindow):
                         i_col += 1
                     continue
                 self.textbox[word_id].append(myQLineEdit(self, i_row, i_col))
-                self.textbox[word_id][i].move(int(self.cw_loc[0] + i_col * self.cw_len), int(self.cw_loc[1] + i_row * self.cw_len))
+                self.textbox[word_id][i].move(self.cw_loc[0] + i_col * self.cw_len,
+                                              self.cw_loc[1] + i_row * self.cw_len)
                 self.textbox[word_id][i].resize(self.cw_len - 1, self.cw_len - 1)
                 self.textbox[word_id][i].setAlignment(Qt.AlignCenter)
                 self.textbox[word_id][i].setFont(QFont("Arial", 16))
@@ -154,6 +185,7 @@ class gameWindow(QMainWindow):
                 self.textbox[word_id][i].setStyleSheet("border: 0.5px solid %s;" % self.tbEdgeColor)
                 my_validator = QRegExpValidator(my_regex, self.textbox[word_id][i])
                 self.textbox[word_id][i].setValidator(my_validator)
+                self.textbox[word_id][i].setEnabled(True)
                 flag[i_row][i_col] = [word_id, i]
                 if self.cw.sortedList[word][2]['dir']:
                     i_row += 1
@@ -169,54 +201,47 @@ class gameWindow(QMainWindow):
         if self.testMode: print('textmap: ', self.textbox_map)
 
     def getNextGame(self):
-        MAX_WORD_NUM = 8  # 一次最多选几个词
-        MIN_WORD_NUM = 5  # 一个填词游戏中最少几个词
-
-        # 根据单词列表生成填词游戏 （此为示例，待完成生词本接口）
-
-        okay = False
-        wordList = getWordList(MAX_WORD_NUM)
-        # print(wordList)
-
-        while not okay:
-            cw = getBestCrossword(wordList)  # 根据要求生成较优的填词游戏
-            if cw.placed.__len__() >= MIN_WORD_NUM:
-                okay = True
-            else:
-                wordList = getWordList(MAX_WORD_NUM)
-
+        '''
+        加载下一轮游戏
+        '''
         self.clearAll()
-        # app = QApplication(sys.argv)
-        cw.display()
-
+        myGame = gameSystem(self.WORD_DICT)
+        cw = myGame.createGameFromStudy(self.WORD_DICT, self.errorWin)
+        # cw = createGameFromAllWord()
         self.cw = cw
+        self.close()
+        self.initUI(cw, self.WORD_DICT, self.errorWin)
         self.show()
-        self.showCrossword()
-        QApplication.processEvents()
+        # QApplication.processEvents()
 
     def clearAll(self):
-        self.showAns.deleteLater()
-        self.hideAns.deleteLater()
-        self.checkAns.deleteLater()
-        self.nextGame.deleteLater()
+        '''
+        清除上一轮游戏的控件
+        '''
         self.dispDef.deleteLater()
-        self.label.deleteLater()
-        self.showCrossword()
+
+        for item in self.label:
+            item.deleteLater()
 
         for item in self.textbox:
             for textbox in item:
                 textbox.deleteLater()
 
+        if 'hideAns' in vars(self): self.hideAns.setVisible(False)
+        if 'showAns' in vars(self): self.showAns.setVisible(False)
         self.textbox = [[] for i in range(len(self.cw.sortedList))]
+        self.label = []
+        self.checkAns.setEnabled(True)
 
     def checkAnswer(self):
         '''
-        检查答案
+        检查并显示答案
         :return:
         '''
-        print('检查答案！')
+        if self.testMode: print('检查答案！')
         cw_height = self.cw.nRow
         cw_width = self.cw.nCol
+        self.checkAns.setEnabled(False)
         for i_row in range(cw_height):
             for i_col in range(cw_width):
                 # 逐个生成格子
@@ -225,17 +250,14 @@ class gameWindow(QMainWindow):
                     i = self.textbox_map[i_row][i_col][1]
                     ans = self.cw.crossword[i_row][i_col]
                     usr_input = self.textbox[word_id][i].text()
-
+                    self.textbox[word_id][i].setStyleSheet("color:black; ")
                     if usr_input.upper() != ans.upper():
                         self.textbox[word_id][i].setStyleSheet("border: 0.5px solid %s; "
+                                                               "color:red; "
                                                                "background-color:rgba(255,225,225);" % self.tbEdgeColor)
-                        if usr_input:
-                            self.textbox[word_id][i].setStyleSheet("border: 0.5px solid %s;"
-                                                                   "color:red; "
-                                                                   "background-color: rgb(255,225,225);"% self.tbEdgeColor)
-                    else:
-                        self.textbox[word_id][i].setStyleSheet("border: 0.5px solid %s;"
-                                                               "background-color: rgb(255,255,255)"% self.tbEdgeColor)
+
+                    self.textbox[word_id][i].setText(self.cw.crossword[i_row][i_col])
+                    self.textbox[word_id][i].setEnabled(False)
 
     def showAnswer(self):
         '''
@@ -276,13 +298,15 @@ class gameWindow(QMainWindow):
         '''
         d_x = 2
         d_y = -9
+        self.label = []
         for word in self.cw.sortedList:
-            self.label = QLabel(self)
-            self.label.setText(str(self.cw.sortedList[word][2]['order']))
+            label = QLabel(self)
+            label.setText(str(self.cw.sortedList[word][2]['order']))
             x = int(self.cw_loc[0] + self.cw.sortedList[word][2]['startPos'][1] * self.cw_len)
             y = int(self.cw_loc[1] + self.cw.sortedList[word][2]['startPos'][0] * self.cw_len)
-            self.label.move(int(x + d_x), int(y + d_y))
-            self.label.setFont(QFont("Simsun", 7))
+            label.move(int(x + d_x), int(y + d_y))
+            label.setFont(QFont("Simsun", 7))
+            self.label.append(label)
 
     def showDefinition(self, defCross, defDown):
         '''
@@ -310,12 +334,20 @@ class gameWindow(QMainWindow):
 
         self.dispDef = QLabel(self)
         self.dispDef.setText(text)
-        self.dispDef.setGeometry(QRect(int(self.def_loc[0] + shift_def_w), int(self.def_loc[1]), self.def_w - shift_def_w, self.def_h))
+        self.dispDef.setGeometry(
+            QRect(int(self.def_loc[0] + shift_def_w), int(self.def_loc[1]), self.def_w - shift_def_w, self.def_h))
         self.dispDef.setWordWrap(True)
-        self.dispDef.setAlignment(Qt.AlignTop)
-        self.dispDef.setFont(QFont("Simsun", 16))
+        self.dispDef.setAlignment(Qt.AlignVCenter)
+        self.dispDef.setFont(QFont("Simsun", 12))
+        self.dispDef.setStyleSheet('''
+                                    QLabel{border:none;color:white;font-weight:00;
+                                        }
+                                ''')
 
     def keyPressEvent(self, a0):
+        '''
+        方向键控制光标移动
+        '''
         if a0.key() == Qt.Key_Up:
             tmp_col = self.current_focus[1]
             tmp_row = self.current_focus[0] - 1
@@ -355,5 +387,3 @@ class gameWindow(QMainWindow):
         print("Context menu requested!!")
         super(gameWindow, self).contextMenuEvent(e)
 
-    def onWindowTitleChange(self, s):
-        self.setWindowTitle(s)
