bc179b692abf2f048582cc51d040aed02b653d3d
wuqian <wu_qi_an@163.com>
2019-12-19 21:56:59 +0800

    add Test_Game to UnitTest

--- UnitTest.py
+++ UnitTest.py
@@ -1,5 +1,6 @@
 from WordDict.UnitTest import Test_WordDict
 from StudyPlan.test_studyPlan import Test_StudyPlan
+from game.test import Test_Game
 import unittest
 
 if __name__ == '__main__':
@@ -8,3 +9,4 @@ if __name__ == '__main__':
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(Test_StudyPlan))
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
+    Test_Game()
